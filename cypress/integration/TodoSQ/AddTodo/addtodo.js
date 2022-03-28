0c8879d837675c61551850945b1f76ae321b09d7
César Subtil <cesar.subtil.2000@gmail.com>
2022-03-28 17:07:28 +0100

    Inicial AddTodo Implementation

---
+++ cypress/integration/TodoSQ/AddTodo/addtodo.js
@@ -0,0 +1,21 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+const url = 'http://localhost:8080'
+var todo = ''
+
+Given('I open the main page', () => {
+ cy.visit(url)
+})
+
+When('I write {string} in the text field', (string) => {
+ todo = string;
+ cy.get('input').type(string)
+})
+
+When('I press the Add button', () => {
+ cy.get('button').click()
+})
+
+Then('I see that it was added to the list', () => {
+ cy.get('li').contains(todo)
+})
--- cypress/integration/project47/Test.feature
+++
@@ -1,6 +0,0 @@
-Feature: Open Main Page
-    I want to open the main page of the website
-
-    Scenario: Opening Main Page
-        Given I open the main page
-        Then I see "Welcome" in the title
